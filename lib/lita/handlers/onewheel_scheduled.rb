0e7a987df043173cd64f4990b019b962ecb0ecbc
Andrew Kreps <andrew.kreps@gmail.com>
2017-09-17 11:47:29 -0700

    Debug logging.

--- lib/lita/handlers/onewheel_scheduled.rb
+++ lib/lita/handlers/onewheel_scheduled.rb
@@ -10,12 +10,14 @@ module Lita
       def start(response)
         every(5) do |timer|
           response.reply("This is your 60 second reminder!")
+          Lita.logger.debug "Timer running, X = #{@x}"
           timer.stop if @x
         end
       end
 
       def end(response)
         @x = true
+        Lita.logger.debug "Timer stopping, X = #{@x}"
         response.reply "Timer stopped."
       end
 
