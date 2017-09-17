39a9bcc66bd59fa0d69e883f33193d996f04bf34
Andrew Kreps <andrew.kreps@gmail.com>
2017-09-17 11:57:51 -0700

    Ok class vars it is

--- lib/lita/handlers/onewheel_scheduled.rb
+++ lib/lita/handlers/onewheel_scheduled.rb
@@ -5,22 +5,22 @@ module Lita
       route /^timerstart$/, :start
       route /^timerend$/, :end
 
-      @x = false
+      @@x = false
 
       def start(response)
         Lita.logger.debug 'Starting timer!'
         every(5) do |timer|
           Lita.logger.debug 'Time'
-          Lita.logger.debug "Timer running, X = #{x}"
-          puts "Timer running, X = #{x}"
-          timer.stop if x == true
+          Lita.logger.debug "Timer running, X = #{@@x}"
+          puts "Timer running, X = #{@@x}"
+          timer.stop if @@x == true
           response.reply('This is your 5 second reminder!')
         end
       end
 
       def end(response)
-        x = true
-        Lita.logger.debug "Timer stopping, X = #{x}"
+        @@x = true
+        Lita.logger.debug "Timer stopping, X = #{@@x}"
         response.reply 'Timer stopped.'
       end
 
