d7beb7f1ee4865e6b046c5117306126464dd0d70
Andrew Kreps <andrew.kreps@gmail.com>
2017-09-17 11:51:35 -0700

    Instance vars even

--- lib/lita/handlers/onewheel_scheduled.rb
+++ lib/lita/handlers/onewheel_scheduled.rb
@@ -8,17 +8,17 @@ module Lita
       @x = false
 
       def start(response)
-        every(5) do |timer|
-          response.reply("This is your 60 second reminder!")
-          Lita.logger.debug "Timer running, X = #{self.x}"
-          timer.stop if self.x
+        every(1) do |timer|
+          response.reply('This is your 1 second reminder!')
+          Lita.logger.debug "Timer running, X = #{x}"
+          timer.stop if x
         end
       end
 
       def end(response)
         self.x = true
-        Lita.logger.debug "Timer stopping, X = #{self.x}"
-        response.reply "Timer stopped."
+        Lita.logger.debug "Timer stopping, X = #{x}"
+        response.reply 'Timer stopped.'
       end
 
       Lita.register_handler(self)
